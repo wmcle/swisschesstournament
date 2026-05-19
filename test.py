1cfe942860428cd743e76c7d97d705908df04c53
Waltenydsam Lemos <waltenydsam.lemos@ipnet.cloud>
2026-05-19 10:49:25 -0300

    sanitizacao de arquivos de teste e debug

--- test.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-def main(page: ft.Page):
-    dlg = ft.AlertDialog(title=ft.Text("Test"))
-    def show_dlg(e):
-        page.dialog = dlg
-        dlg.open = True
-        page.update()
-    page.add(ft.ElevatedButton("Show", on_click=show_dlg))
-ft.app(target=main)
--- test_color.py
+++
@@ -1,4 +0,0 @@
-import flet as ft
-def main(page: ft.Page):
-    page.add(ft.IconButton(icon="edit", icon_color="blue400"))
-ft.app(target=main)
